0253564b132f7599deb6d0671e727329e99b1e97
Ashu247 <54639390+Ashu247@users.noreply.github.com>
2019-09-09 16:15:32 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,3 +3,27 @@ variable "aws.region" {
 description= "region for aws"
 default= "us-west-2"
 }
+
+variable "vpc_cidr" {
+description= "cidr for the VPC"
+default= "10.0.0.0/16"
+  }
+variable "public_subnet_cidr"
+decription= "cidr for public subnet"
+default= "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+description-= "cidr for private subnet"
+default= "10.0.1.0/24"
+  }
+
+variable "ami" {
+description= "Amazon ubuntu image"
+default= "ami-0b37e9efc396e4c38 (64-bit x86"
+  }
+
+variable "key_path" {
+  description = "SSH Public Key path"
+  default = "public_key"
+}
